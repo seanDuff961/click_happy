59ac2c2462802badac25161ea8cdc55af7dd019b
Sean Duff <gatewaywebdesign18@gmail.com>
2019-11-22 21:10:59 -0600

    modify DB URL env

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(process.env.DATABASE_URI);
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 const User = require('./models/User')(sequelize);
 
